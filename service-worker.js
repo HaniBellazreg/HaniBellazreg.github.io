3309889836c470cbab525f7dcfc0c00d3173e377
HaniBellazreg <hanibellazreg@gmail.com>
2019-02-15 14:59:38 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+var cacheName='heni-test-1.0.0.0';
+var filesToCache=[
+    "./",
+    "./main.js",
+    "index.html"
+];
+
+self.addEventListener('install', function(e){
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req){
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
